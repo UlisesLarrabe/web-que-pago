7bc3eeba2095b70aaf5a7d6a70b41b81d8d0f197
Ulises <larrabeulises@gmail.com>
2024-11-29 19:08:17 -0300

    Add image utility module for streaming service logos

---
+++ src/utils/images.js
@@ -0,0 +1,12 @@
+export const IMAGES = {
+  netflix: "logos/netflix.webp",
+  hbomax: "logos/hbomax.webp",
+  disneyplus: "logos/disneyplus.webp",
+  amazonprime: "logos/amazonprime.webp",
+  youtubepremium: "logos/youtubepremium.webp",
+  crunchyroll: "logos/crunchyroll.webp",
+  flow: "logos/flow.webp",
+  clarovideo: "logos/clarovideo.webp",
+};
+
+export const ARRAY_IMAGES = Object.keys(IMAGES).map((key) => IMAGES[key]);
