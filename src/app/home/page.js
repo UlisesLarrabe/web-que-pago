3e5930daff790135fbf0afaeb7ff3dcd8938b219
Ulises <larrabeulises@gmail.com>
2024-11-29 19:07:38 -0300

    Add Home page component with authentication redirect

---
+++ src/app/home/page.js
@@ -0,0 +1,19 @@
+import { HomePage } from "@/components/HomePage";
+import { UserProvider } from "@/context/userContext";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const allCookies = await cookies();
+  const hasCookies = allCookies.has("access_token");
+  if (!hasCookies) {
+    redirect("/login");
+  }
+
+  return (
+    <UserProvider>
+      <HomePage />
+    </UserProvider>
+  );
+};
+export default Home;
