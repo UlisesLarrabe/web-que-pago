5ba441b5a478b784121d42428117e7d6deaf208b
Ulises <larrabeulises@gmail.com>
2024-11-29 19:08:04 -0300

    Add Footer component with author attribution

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 absolute p-2">
+      <p className="text-center text-white">
+        Hecho por{" "}
+        <a
+          href="https://github.com/UlisesLarrabe"
+          target="_blank"
+          className="text-[#FFC470]"
+        >
+          Ulises Alejandro Larrabe
+        </a>
+      </p>
+    </footer>
+  );
+};
+export default Footer;
